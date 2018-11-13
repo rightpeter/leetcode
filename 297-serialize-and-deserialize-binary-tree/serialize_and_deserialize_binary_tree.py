a9a2a909390bc68490586a5ce34137683fb62292
Rightpeter <rightpeter.lu@gmail.com>
2018-11-12 22:45:29 -0600

    297

--- 297-serialize-and-deserialize-binary-tree/serialize_and_deserialize_binary_tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize_and_deserialize_binary_tree.py
@@ -1,64 +1,76 @@
+#!/usr/bin/env python3
+
+
 class TreeNode(object):
+
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
 
-import json
 
 class Codec:
 
     def serialize(self, root):
         """Encodes a tree to a single string.
-        
         :type root: TreeNode
         :rtype: str
         """
         ans = []
         queue = []
         if root is not None:
-            queue.append([0, root])
-        
+            queue.append(root)
+
         while len(queue) > 0:
             task = queue.pop(0)
-            ans.append([task[0], task[1].val])
-            if task[1].left is not None:
-                queue.append([task[0]*2+1, task[1].left])
-            if task[1].right is not None:
-                queue.append([task[0]*2+2, task[1].right])
-        
-        return json.dumps(ans)
-        
+            if task is not None:
+                ans.append(str(task.val))
+                queue.append(task.left)
+                queue.append(task.right)
+            else:
+                ans.append("null")
+
+        if len(ans) > 0:
+            i = len(ans) - 1
+            while i >= 0 and ans[i] == 'null':
+                i -= 1
+            ans = ans[:i + 1]
+        return '[' + ','.join(ans) + ']'
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
-        
         :type data: str
         :rtype: TreeNode
         """
-        json_tree = json.loads(data)
-
-        hash_table = {}
+        data = data[1:-1].split(',')
         root = None
+        queue = []
 
         # print("data: ", data)
-        if len(json_tree) > 0:
-            node = json_tree.pop(0)
-            root = TreeNode(node[1])
-            hash_table[0] = root
-        
-        for node in json_tree:
-            parent = (node[0] + 1) / 2 - 1 if node[0] % 2 == 1 else node[0] / 2 - 1
-            # print("parent: ", parent)
-            tree_node = TreeNode(node[1])
-            hash_table[node[0]] = tree_node
-            if node[0] % 2 == 1:
-                hash_table[parent].left = tree_node
-            else:
-                hash_table[parent].right = tree_node
-        
+        num = data.pop(0)
+        if num != '':
+            root = TreeNode(int(num))
+            queue.append(root)
+
+        while len(data) > 0 and len(queue) > 0:
+            node = queue.pop(0)
+            if len(data) > 0:
+                num = data.pop(0)
+                if num != 'null':
+                    left = TreeNode(int(num))
+                    # print("add {} to left of {}".format(num, node.val))
+                    node.left = left
+                    queue.append(left)
+            if len(data) > 0:
+                num = data.pop(0)
+                if num != 'null':
+                    right = TreeNode(int(num))
+                    # print("add {} to left of {}".format(num, node.val))
+                    node.right = right
+                    queue.append(right)
+
         return root
-        
+
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
--- 297-serialize-and-deserialize-binary-tree/serialize_and_deserialize_binary_tree_test.py
+++ 297-serialize-and-deserialize-binary-tree/serialize_and_deserialize_binary_tree_test.py
@@ -1,10 +1,13 @@
 import unittest
 from serialize_and_deserialize_binary_tree import Codec
+from serialize_and_deserialize_binary_tree import TreeNode
+
 
 class TestBinaryTree(unittest.TestCase):
+
     def test_1(self):
         codec = Codec()
-        tree_str = '[[0, 1], [1, 2], [2, 3], [3, 4], [4, 5]]'
+        tree_str = "[1,2,3,null,null,4,5]"
         self.assertEqual(codec.serialize(codec.deserialize(tree_str)), tree_str)
 
     def test_2(self):
@@ -14,8 +17,27 @@ class TestBinaryTree(unittest.TestCase):
 
     def test_3(self):
         codec = Codec()
-        tree_str = '[[0, 5], [1, 4], [2, 7], [3, 3], [5, 2], [7, -1], [11, 9]]'
+        tree_str = '[5,4,7,3,null,2,null,-1,null,null,null,9]'
         self.assertEqual(codec.serialize(codec.deserialize(tree_str)), tree_str)
+
+    def test_4(self):
+        codec = Codec()
+        self.assertEqual(codec.serialize(None), '[]')
+
+    def test_5(self):
+        codec = Codec()
+        self.assertEqual(codec.deserialize('[]'), None)
+
+    def test_6(self):
+        codec = Codec()
+        node = TreeNode(3)
+        self.assertEqual(codec.serialize(node), '[3]')
+
+    def test_7(self):
+        codec = Codec()
+        self.assertEqual(codec.deserialize(codec.serialize(None)), None)
+
 
 if __name__ == '__main__':
     unittest.main()
+    # TestBinaryTree().test_5()
