83e12438c5d5429beede26b7cdacab0d72535ac3
Lihao Lu <lihaolu@amazon.com>
2019-07-25 14:49:22 -0700

    83 remove duplicates from sorted list

---
+++ 83-remove-duplicates-from-sorted-list/remove_duplicates_from_sorted_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from node_list_helper import ListNode
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        curr = head
+        while curr:
+            while curr.next and curr.val == curr.next.val:
+                curr.next = curr.next.next
+            curr = curr.next
+        return head
